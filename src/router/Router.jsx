abf40e911437c48ba95d835f93d6029c886aa417
kerime abanoz <krmeabnz@gmail.com>
2023-01-16 23:13:52 +0300

    router started

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
