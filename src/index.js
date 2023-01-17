b1f8f9d877b16352c9fd910cdc82f03181d950ae
kerime abanoz <krmeabnz@gmail.com>
2023-01-17 16:07:38 +0300

    all the files deleted

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import 'tw-elements';
-import "react-toastify/dist/ReactToastify.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
